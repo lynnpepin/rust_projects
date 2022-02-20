4cb10eec4737001416e520973f728dc6cbf28842
lynnpepin <lynnpepin@tutanota.com>
2022-02-19 20:58:18 -0500

    Added tests, project still confusing

---
+++ countsort/README.md
@@ -0,0 +1,5 @@
+# Countsort.md
+
+The code in `lib.rs` and `main.rs` is mainly good, but I can't make Python work while `use`ing properly. (Need cdylib!)
+
+This is a tough spot for many others (see https://github.com/rust-lang/cargo/issues/7885). No idea how to make it work yet!
--- countsort/src/lib.rs
+++ countsort/src/lib.rs
@@ -23,11 +23,12 @@ use pyo3::prelude::*;
 /// # Examples
 /// 
 /// ```
+/// use countsort::count_and_sort;
 /// let vals: Vec<u32>  = vec![2, 0, 4, 5, 7, 7, 0, 6, 1, 7, 6];
 /// let w: u8 = 3;
 /// let sorted_vals = count_and_sort(vals, w);
 /// let test_vals: Vec<u32> = vec![0, 0, 1, 2, 4, 5, 6, 6, 7, 7, 7];
-/// assert_eq!(vals, test_vals);
+/// assert_eq!(sorted_vals, test_vals);
 /// ```
 /// 
 /// (I don't know if this test actually works-- I couldn't get rustdoc test to work yet!)
